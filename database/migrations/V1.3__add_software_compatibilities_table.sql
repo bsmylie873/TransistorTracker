d8bb4f37f8a77a4374543b0ed54183ed29cbcb55
Brandon Smylie <brandon.smylie@unosquare.com>
2025-02-04 09:53:14 -0500

    New migrations added.

---
+++ database/migrations/V1.3__add_software_compatibilities_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE SOFTWARE_COMPATIBILITY_LEVELS (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE SOFTWARE_COMPATIBILITIES (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    software_id INT NOT NULL,
+    part_id INT,
+    device_id INT,
+    software_compatibility_level_id INT NOT NULL,
+    FOREIGN KEY (software_id) REFERENCES SOFTWARE(id),
+    FOREIGN KEY (part_id) REFERENCES PARTS(id),
+    FOREIGN KEY (device_id) REFERENCES DEVICES(id),
+    FOREIGN KEY (software_compatibility_level_id) REFERENCES SOFTWARE_COMPATIBILITY_LEVELS(id)
+);
---
+++ database/migrations/V1.4__add_software_compatibility_ref_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO SOFTWARE_COMPATIBILITY_LEVELS (id, level_name, description) VALUES
+(1, 'Full', 'The software is fully compatible with the device.'),
+(2, 'Partial', 'The software is partially compatible with the device. Some features may not work.'),
+(3, 'None', 'The software is not compatible with the device.');
